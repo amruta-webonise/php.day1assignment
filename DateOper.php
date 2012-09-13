3389d530953b6226d3e1bc8780dc1f9963320f2f
webonise <webonise@webonise-desktop.(none)>
2012-09-13 15:18:48 +0530

    ans 21- date in array format

--- DateOper.php
+++ DateOper.php
@@ -4,7 +4,15 @@
 $today = date("d.m.y");
 echo "Answer 7] today is $today";
 
+echo '<br/>';
+echo '<br/>';
+echo '<br/>';
+echo '<br/>';
 
-
+echo "Answer 21] today is in array format $today";
+$today = getdate();
+echo '<pre>';
+print_r($today);
+echo '</pre>';
 ?>
 
