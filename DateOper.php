66a00e8d6729467ac0072d994489a73bf896faa9
webonise <webonise@webonise-desktop.(none)>
2012-09-13 15:52:11 +0530

    comparing two dates and finding the difference

--- DateOper.php
+++ DateOper.php
@@ -27,6 +27,30 @@ $date = date("Y-m-d", strtotime($start . "+20 day"));
 echo "Answer 20] today is $today.";
 echo '<br/>';
 echo "20 days later $date";
+echo '<br/>';
+echo '<br/>';
+echo '<br/>';
+echo '<br/>';
+
+
+//comparing two dates and finding the difference
+ $first_date = "12-4-2010"; 
+$second_date = "12-5-2011";
+$first = strtotime($first_date);
+ $second = strtotime($second_date);
+$days = (strtotime($second_date)-strtotime($first_date))/(60*60*24);
+ if ($first > $second) 
+{ 
+	echo "$first_date is greater than $second_date and will come later.";
+	echo '<br/>';
+	echo "difference between the two dates is of $days";
+} 
+else 
+{ 
+	echo "$first_date is smaller than $second_date and will come before.";
+	echo '<br/>';
+	echo "difference between the two dates is of $days";
+} 
 
 ?>
 
