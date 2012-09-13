1c7e438da22a5ce00e896ac6c3058f815c34c4d9
webonise <webonise@webonise-desktop.(none)>
2012-09-13 15:29:14 +0530

    ans 20- date after 20 days from today

--- DateOper.php
+++ DateOper.php
@@ -9,10 +9,24 @@ echo '<br/>';
 echo '<br/>';
 echo '<br/>';
 
+//date in array format
 echo "Answer 21] today is in array format $today";
 $today = getdate();
 echo '<pre>';
 print_r($today);
 echo '</pre>';
+
+echo '<br/>';
+echo '<br/>';
+echo '<br/>';
+echo '<br/>';
+
+//date after 20 days
+$today = date("Y-m-d");
+$date = date("Y-m-d", strtotime($start . "+20 day"));
+echo "Answer 20] today is $today.";
+echo '<br/>';
+echo "20 days later $date";
+
 ?>
 
